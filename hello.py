235024d44f5db37eb8df8703b81a00d2c20df396
marianadeem-10xe <108668196+marianadeem-10xe@users.noreply.github.com>
2023-02-09 12:03:31 +0500

    added hello.py

---
+++ hello.py
@@ -0,0 +1,12 @@
+"""Module docstring goes here"""
+def add(n_1, n_2):
+    """Function docstring goes here"""
+    return n_1 + n_2
+
+
+print("Hello World")
+X = 10
+Y = 20
+z = 3 + 3 + X * Y
+print(X, Y)
+print(add(X, Y))
